392d4256d73e792a91e9106f86ca6a3f8357a27b
angepili2 <angepili@icloud.com>
2016-11-01 12:31:35 +0100

    app

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    cleanCss        = require('gulp-clean-css'),
+    useref          = require('gulp-useref'),
+    image           = require('gulp-image'),
+    browserSync     = require('browser-sync');
+
+gulp.task('image', function () {
+  gulp.src('./app/assets/img/*')
+    .pipe(gulp.dest('./dist/assets/img'));
+});
+
+gulp.task('html',function(){
+    gulp.src('./app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('style',function(){
+    gulp.src('./app/assets/sass/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('./dist/assets/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync',function(){
+    browserSync.init();
+});
+
+gulp.task('watch',['style','html','image'],function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch('./app/assets/sass/**/*.scss',['style']);
+    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default',['style','html','image']);
